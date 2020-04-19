aa7238510729dd8614a460e769abb446981032d4
seflmsw <ecsimsw@gmail.com>
2020-04-19 17:57:38 +0900

    다리를 건너는 트럭

--- src/Programmers_AlgorithmTest/Stack_Queue/다리를건너는트럭.java
+++ src/Programmers_AlgorithmTest/Stack_Queue/다리를건너는트럭.java
@@ -1,71 +1,111 @@
 package Programmers_AlgorithmTest.Stack_Queue;
 
-import java.sql.Struct;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
+import java.nio.charset.IllegalCharsetNameException;
 import java.util.LinkedList;
+import java.util.Queue;
+
 public class 다리를건너는트럭 {
-    public static void main(String[] args) {
-        System.out.println(Solution());
-    }
+    static public void main(String[] args){
+        int bridge_length = 100;
+        int weight = 100;
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
 
-    static int Solution(){
-        int bridge_length = 2;
-        int weight = 10;
-        int[] truck_weights = {7,4,5,6};
+        Solution otherCode = new Solution();
+        System.out.println(otherCode.solution(bridge_length, weight, truck_weights));
 
+    /*
+        int bridge_length = 100;
+        int weight = 100;
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
 
-        Queue<Truck> onBridge = new LinkedList<Truck>();
+        Queue<Truck> onBridge = new LinkedList<>();
 
-        int onBridgeWeight =0;
-        int answer =0;
-        int indexTruck=0;
+        int onBridgeWeight = 0;
+        int index=0;
+        int answer=0;
 
         while(true){
+            System.out.println("a :"+answer);
+            if(onBridge.size() !=0)
+                System.out.println("w :" + onBridge.peek().hashCode()+ "l : "+onBridge.peek().life);
 
-            System.out.println(onBridgeWeight+"  "+answer);
+            if(index < truck_weights.length){
+                if(onBridgeWeight + truck_weights[index] <= weight){
+                    System.out.println("offer");
+                    onBridge.offer(new Truck(truck_weights[index]));
+                    onBridgeWeight += truck_weights[index];
+                    index++;
+                }
+            }
+            else{ if(onBridge.isEmpty()){ break; } }
+
+            for(Truck t : onBridge){ t.life++; }
+
+            if(onBridge.peek().life == bridge_length){
+                onBridgeWeight-=onBridge.peek().weight;
+                onBridge.poll();
+            }
             answer++;
-
-            for(Truck t : onBridge){
-                t.life++;
-                if(t.life>bridge_length-1){
-                    onBridgeWeight -= t.weight;
-                    onBridge.poll();
-                }
-            }
-
-            if(indexTruck < truck_weights.length){
-                if(onBridgeWeight + truck_weights[indexTruck] < weight) {
-                    onBridge.add(new Truck(truck_weights[indexTruck]));
-                    onBridgeWeight+= truck_weights[indexTruck];
-                    indexTruck++;
-                }
-            }
-            else{
-                if(onBridge.isEmpty()){
-                    return answer;
-                }
-            }
         }
+
+        System.out.println(answer);
+
+         */
     }
 }
 
 class Truck{
+    int life=0;
     int weight;
-    public int life=0;
 
-    Truck(int w){
+    public Truck(int w){
         this.weight = w;
     }
 }
 
-/*
-트럭 여러 대가 강을 가로지르는 일 차선 다리를 정해진 순으로 건너려 합니다.
-모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다.
-트럭은 1초에 1만큼 움직이며, 다리 길이는 bridge_length이고 다리는 무게 weight까지 견딥니다.
-※ 트럭이 다리에 완전히 오르지 않은 경우, 이 트럭의 무게는 고려하지 않습니다.
+class Solution {
+    class Truck {
+        int weight;
+        int move;
 
-예를 들어, 길이가 2이고 10kg 무게를 견디는 다리가 있습니다.
-무게가 [7, 4, 5, 6]kg인 트럭이 순서대로 최단 시간 안에 다리를 건너려면 다음과 같이 건너야 합니다.
- */
+        public Truck(int weight) {
+            this.weight = weight;
+            this.move = 1;
+        }
+
+        public void moving() {
+            move++;
+        }
+    }
+
+    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
+
+        for (int t : truckWeights) { waitQ.offer(new Truck(t)); }
+
+        int answer = 0;
+        int curWeight = 0;
+
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+            }
+
+            for (Truck t : moveQ) { t.moving();}
+
+            if (moveQ.peek().move > bridgeLength) {
+                Truck t = moveQ.poll();
+                curWeight -= t.weight;
+            }
+
+
+        }
+
+        return answer;
+    }
+}
