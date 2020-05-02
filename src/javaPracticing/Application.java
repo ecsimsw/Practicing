a1f50195249f0c2a17ad155beffec32a56a0493a
seflmsw <ecsimsw@gmail.com>
2020-05-02 23:57:52 +0900

    스택, 큐 / 주식 가격

--- src/javaPracticing/Application.java
+++ src/javaPracticing/Application.java
@@ -4,49 +4,24 @@ package javaPracticing;
 import java.util.*;
 
 public class Application {
-    public static void main(String[] args){
-        LinkedList<Integer> linkedlist = new LinkedList<>();
+    public static void main(String[] args) {
 
-        linkedlist.offer(1);
-        linkedlist.add(2);
-        linkedlist.addFirst(3);
-        linkedlist.addLast(4);
-        linkedlist.addAll(linkedlist);
+        HashSet<Student> hs = new HashSet<>();
 
-        for(int i : linkedlist){
-            System.out.print(i);
-        }
-        System.out.println();
+        Student a = new Student(1, "A");
+        Student b = new Student(2, "B");
+        Student A = new Student(1, "A");
 
-        HashSet<Integer> hashSet1 = new HashSet<>(linkedlist);
-        for(int i : hashSet1){
-            System.out.print(i);
-        }
+        System.out.println(a.equals(A));
+        System.out.println(a.hashCode());
+        System.out.println(A.hashCode());
 
-        System.out.println();
+        hs.add(a);
+        hs.add(b);
+        hs.add(A);
 
-
-        HashSet<Integer> hashSet2 = new LinkedHashSet<>(linkedlist);
-        for(int i : hashSet2){
-            System.out.print(i);
-        }
-
-        System.out.println();
-
-Student A = new Student(1,"apple");
-Student B = new Student(2,"banana");
-Student C = new Student(3,"grape");
-Student D = new Student(1,"apple");
-
-HashSet<Student> class1 = new HashSet<>();
-
-class1.add(A);
-class1.add(B);
-class1.add(C);
-class1.add(D);
-
-        for(Student s : class1){
-            System.out.print(s.name+ "   ");
+        for(Student s : hs){
+            System.out.print(s.name);
         }
     }
 }
@@ -68,8 +43,11 @@ class Student{
         return number == student.number && Objects.equals(name, student.name);
     }
 
+    /*
     @Override
     public int hashCode() {
         return Objects.hash(number, name);
     }
+    */
+
 }
