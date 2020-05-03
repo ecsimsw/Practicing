5e18869c49b2593d18ea436f6c48bdaf7073ffbf
seflmsw <ecsimsw@gmail.com>
2020-05-04 06:36:35 +0900

    와일드 카드

--- src/javaPracticing/Application.java
+++ src/javaPracticing/Application.java
@@ -1,33 +1,57 @@
 package javaPracticing;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Application {
-    private final String str="";
-
     public static void main(String[] args) {
 
-        Application.<Integer>getID(123);
+        Apple apple1 = new Apple();
+        Apple apple2 = new Apple();
 
-    }
-    public static <T> T getID(T id){return (T)id;}
-}
-class Parent{
-   static public void sayHi(){
-       System.out.println("parent");
-   }
-}
+        FruitBox<Apple> appleBox = new FruitBox<>();
 
-class Child extends Parent{
-    static public void sayHi(){
-        System.out.println("child");
+        appleBox.add(apple1);
+        appleBox.add(apple2);
+
+        System.out.println(Juicer.makeJuice(appleBox));
     }
 }
+class Juice{}
 
-class Student<T>{
-    T name;
-
-    public T getName(T name){ this.name = name; return name; }
-    public <S> T toT1(S id){return (T)id;}
-    public <T> T getId(T id){return (T)id;}
+class Juicer{
+    static <T extends Fruit> String makeJuice(FruitBox<T> box){
+        String tmp = "";
+        for(Fruit f : box.list){tmp +="apple  / ";}
+        tmp+="juice";
+        return tmp;
+    }
 }
+
+class Fruit{ }
+class Apple extends Fruit{}
+
+class FruitBox<T extends Fruit> extends Box<T> {}
+
+class Box<T>{
+    ArrayList<T> list = new ArrayList<>();
+    void add(T item){list.add(item);}
+    T get(int i){return list.get(i);}
+}
+
+/*
+class Card<T>{
+    T cardNumber;
+
+    public Card(T cardNumber){
+        this.cardNumber = cardNumber;
+    }
+}
+class Student {
+    public void tagCard(Card<?> card) {
+        String a = card.cardNumber.toString();
+        a += "123";
+        System.out.println(card.cardNumber);
+    }
+}
+
+ */
