173e9a2cf0d211aa0a7318e32f9263181f6c0d98
seflmsw <ecsimsw@gmail.com>
2020-05-05 21:46:41 +0900

    2016년

---
+++ src/Programmers_AlgorithmTest/LEVEL2/같은숫자는싫어.java
@@ -0,0 +1,30 @@
+package Programmers_AlgorithmTest.LEVEL2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class 같은숫자는싫어 {
+
+    public int[] solution(int []arr) {
+        int pre = arr[0];
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(pre);
+
+        for(int i=1; i<arr.length; i++){
+            if(pre != arr[i]){
+                pre= arr[i];
+                linkedList.add(pre);
+            }
+        }
+
+        int[] answer = new int[linkedList.size()];
+
+        int index=0;
+        for(int i : linkedList){
+            answer[index] =i;
+        }
+
+        return answer;
+    }
+}
