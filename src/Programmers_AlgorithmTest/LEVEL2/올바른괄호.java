fcfa66ace9cb79601615bbd73b425c6a6e6fdd62
seflmsw <ecsimsw@gmail.com>
2020-05-07 03:40:55 +0900

    올바른 괄호

---
+++ src/Programmers_AlgorithmTest/LEVEL2/올바른괄호.java
@@ -0,0 +1,23 @@
+package Programmers_AlgorithmTest.LEVEL2;
+
+public class 올바른괄호 {
+    public static void main(String[] args){
+
+    }
+    static boolean solution(String s) {
+        boolean answer = true;
+
+        int stack =0;
+        for(int i =0; i<s.length(); i++){
+            if(s.charAt(i)=='('){ stack ++; }
+            else {
+                if(stack>0){ stack--; }
+                else{ return false; }
+            }
+        }
+        answer = (stack == 0);
+
+        return answer;
+    }
+}
+
