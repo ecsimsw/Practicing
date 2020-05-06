79700ee53ed8b93747ed4a2712e78c77726aba26
seflmsw <ecsimsw@gmail.com>
2020-05-07 03:58:18 +0900

    나눠떨어지는숫자배열

--- src/Programmers_AlgorithmTest/LEVEL2/올바른괄호.java
+++ src/Programmers_AlgorithmTest/LEVEL2/올바른괄호.java
@@ -4,20 +4,20 @@ public class 올바른괄호 {
     public static void main(String[] args){
 
     }
-    static boolean solution(String s) {
-        boolean answer = true;
+static boolean solution(String s) {
+    boolean answer = true;
 
-        int stack =0;
-        for(int i =0; i<s.length(); i++){
-            if(s.charAt(i)=='('){ stack ++; }
-            else {
-                if(stack>0){ stack--; }
-                else{ return false; }
-            }
+    int stack =0;
+    for(int i =0; i<s.length(); i++){
+        if(s.charAt(i)=='('){ stack ++; }
+        else {
+            if(stack>0){ stack--; }
+            else{ return false; }
         }
-        answer = (stack == 0);
-
-        return answer;
     }
+    answer = (stack == 0);
+
+    return answer;
+}
 }
 
