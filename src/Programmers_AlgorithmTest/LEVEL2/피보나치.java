496bf21bcc1a77e3e7b686c545cc65a29548c5de
ecsimsw <ecsimsw@gmail.com>
2020-06-07 00:00:01 +0900

    level2

--- src/Programmers_AlgorithmTest/LEVEL2/피보나치.java
+++ src/Programmers_AlgorithmTest/LEVEL2/피보나치.java
@@ -26,4 +26,29 @@ public class 피보나치 {
 
             return r % 1234567;
         }
+
+
+        // 재귀 없이
+    class Solution {
+        public int solution(int n) {
+            if(n==0 || n ==1) return n;
+
+            int i= 2;
+
+            int pre = 1;
+            int ppre = 0;
+            int now = 0;
+
+            while(i<=n){
+                now = pre + ppre;
+                ppre = pre;
+                pre = now;
+                i++;
+            }
+
+            int answer = now;
+
+            return answer;
+        }
+    }
 }
