2d9f66b7d09734573ef81c68c911c3d485934cf3
ecsimsw <ecsimsw@gmail.com>
2020-06-07 00:01:49 +0900

    피보나치 재귀 없이

--- src/Programmers_AlgorithmTest/LEVEL2/피보나치.java
+++ src/Programmers_AlgorithmTest/LEVEL2/피보나치.java
@@ -1,54 +1,25 @@
 package Programmers_AlgorithmTest.LEVEL2;
 
+// 재귀 없이 피보나치
 public class 피보나치 {
+    public int solution(int n) {
+        if(n==0 || n ==1) return n;
 
+        int i= 2;
 
-=
-        public int solution(int n) {
-            int answer = 0;
+        int pre = 1;
+        int ppre = 0;
+        int now = 0;
 
-            answer = func(n) %1234567;
-
-            return answer;
+        while(i<=n){
+            now = pre + ppre;
+            ppre = pre;
+            pre = now;
+            i++;
         }
 
-        public long func(long a) {
-            long[] arr = new long[10000];
+        int answer = now;
 
-            if (a == 0) return 0;
-            if (a == 1) return 1;
-            if (a == 2) return 1;
-
-            if (arr[(int) a] != 0) return arr[a] % 1234567;
-
-            long r = func(a - 1) + func(a - 2);
-            arr[r] = r;
-
-            return r % 1234567;
-        }
-
-
-        // 재귀 없이
-    class Solution {
-        public int solution(int n) {
-            if(n==0 || n ==1) return n;
-
-            int i= 2;
-
-            int pre = 1;
-            int ppre = 0;
-            int now = 0;
-
-            while(i<=n){
-                now = pre + ppre;
-                ppre = pre;
-                pre = now;
-                i++;
-            }
-
-            int answer = now;
-
-            return answer;
-        }
+        return answer;
     }
 }
