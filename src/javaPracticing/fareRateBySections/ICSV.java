e40d97df64ef8a3a5274341a8631375ca52e15d2
ecsimsw <ecsimsw@gmail.com>
2020-07-01 06:34:27 +0900

    algorithm

--- src/javaPracticing/fareRateBySections/ICSV.java
+++
@@ -1,8 +0,0 @@
-package javaPracticing.fareRateBySections;
-
-import java.util.*;
-
-public interface ICSV {
-    List<DTO> loadCSV(String fileName);
-    boolean saveCSV(List<DTO> lines);
-}
--- src/javaPracticing/java20final/Gender1.java
+++
@@ -1,5 +0,0 @@
-package javaPracticing.java20final;
-
-public enum Gender1 {
-	MALE, FEMALE;
-}	//enum 형식의 열거형 문법
