914ff466e8e64367165d72c1ffcbd241fbccc5dd
ecsimsw <ecsimsw@gmail.com>
2020-07-14 22:11:27 +0900

    하루 한알 / b1874_stack_스택수열

--- src/BOJ/b1874_stack_스택수열.java
+++ src/BOJ/b1874_stack_스택수열.java
@@ -11,47 +11,93 @@ public class b1874_stack_스택수열 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+
         Queue<Integer> queue = new LinkedList<>();
         Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-        int n = 1;
-
-        queue.offer(4);
-        queue.offer(3);
-        queue.offer(6);
-        queue.offer(8);
-        queue.offer(7);
-        queue.offer(5);
-        queue.offer(2);
-        queue.offer(1);
+        for(int i=0; i<n; i++) {
+            queue.offer(Integer.parseInt(br.readLine()));
+        }
 
         boolean isAble = true;
+
+        int number =1;
         while(true){
             if(stack.isEmpty()) {
-                System.out.println("push "+n);
-                stack.push(n);
-                n++;
+                sb.append("+\n");
+                stack.push(number);
+                number++;
             }
 
             if(stack.peek() == queue.peek()){
-                System.out.println("pop "+stack.peek());
+                sb.append("-\n");
                 stack.pop();
                 queue.poll();
-                n++;
             }
-
             else if(stack.peek() < queue.peek()){
-                System.out.println("push "+n);
-                stack.push(n);
-                n++;
+                sb.append("+\n");
+                stack.push(number);
+                number++;
             }
-
             else if(stack.peek() > queue.peek()){
                 isAble =false;
                 break;
             }
 
-            if(n > 8) break;
+            if(queue.isEmpty()) break;
         }
+        sb.delete(sb.length()-1, sb.length());
+        if(isAble) System.out.print(sb);
+        else System.out.print("NO");
+    }
+}
+
+class b1874_stack_스택수열_배열 {
+    static public void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++) {
+            queue.offer(Integer.parseInt(br.readLine()));
+        }
+
+        boolean isAble = true;
+
+        int number =1;
+        while(true){
+            if(stack.isEmpty()) {
+                sb.append("+\n");
+                stack.push(number);
+                number++;
+            }
+
+            if(stack.peek() == queue.peek()){
+                sb.append("-\n");
+                stack.pop();
+                queue.poll();
+            }
+            else if(stack.peek() < queue.peek()){
+                sb.append("+\n");
+                stack.push(number);
+                number++;
+            }
+            else if(stack.peek() > queue.peek()){
+                isAble =false;
+                break;
+            }
+
+            if(queue.isEmpty()) break;
+        }
+        sb.delete(sb.length()-1, sb.length());
+        if(isAble) System.out.print(sb);
+        else System.out.print("NO");
     }
 }
