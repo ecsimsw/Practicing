ede5a8a54df1ec0ad374bc1d84eafd90ed2c5616
ecsimsw <ecsimsw@gmail.com>
2020-07-15 17:37:57 +0900

    하루 한알 / b2231_브루트포스_분해합

---
+++ src/BOJ/b2231_브루트포스_분해합.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.Scanner;
+
+public class b2231_브루트포스_분해합 {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum=0;
+        int i= 1;
+        boolean finish = false;
+        for(i=1; i<n; i++){
+            int temp = i;
+            sum =i;
+
+            while(temp>0){
+                sum += temp%10;
+                temp/=10;
+            }
+
+            if(sum == n) {
+                finish = true;
+                break;
+            }
+        }
+
+        if(!finish) System.out.print("0");
+        else{ System.out.print(i); }
+    }
+}
