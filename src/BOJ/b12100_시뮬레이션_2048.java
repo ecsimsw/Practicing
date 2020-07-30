b95902b63f4e630ab201a3ba979d5e07f02cd787
ecsimsw <ecsimsw@gmail.com>
2020-07-31 03:51:31 +0900

    하루 한알 / b12100_시뮬레이션_2048

--- src/BOJ/b12100_시뮬레이션_2048.java
+++ src/BOJ/b12100_시뮬레이션_2048.java
@@ -22,13 +22,10 @@ public class b12100_시뮬레이션_2048 {
         s= board.length;
         g =board[0].length;
 
-        //func(5, board);
-        move(3);
-        printBoard();
-        //System.out.print(max);
+        func(5, board);
 
-        move(4);
-        printBoard();
+        System.out.print(max);
+
     }
 
     static int s;
@@ -40,6 +37,7 @@ public class b12100_시뮬레이션_2048 {
 
     static void func(int n, int[][] board){
         if(n==0){
+            //System.out.println("=========");
             for(int i=0; i<board.length; i++){
                 for(int j=0; j<board[0].length; j++) {
                     if(board[i][j]>max) max = board[i][j];
@@ -48,14 +46,14 @@ public class b12100_시뮬레이션_2048 {
             return;
         }
         for(int i=1; i<5;i++){
-            move(i);
-            int[][] boardOri = copyBoard();
-            func(n-1,board);
+            int[][] boardOri = copyBoard(board);
+            //printBoard(board);
+            func(n-1,move(i, board));
             board = boardOri;
         }
     }
 
-    static int[][] copyBoard(){
+    static int[][] copyBoard(int[][] board){
         int[][] ori = new int[s][g];
 
         for(int i=0; i<s; i++){
@@ -67,7 +65,7 @@ public class b12100_시뮬레이션_2048 {
         return ori;
     }
 
-    static void printBoard(){
+    static void printBoard(int[][] board){
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
                 System.out.print(board[i][j] + " ");
@@ -77,15 +75,17 @@ public class b12100_시뮬레이션_2048 {
         System.out.println();
     }
 
-    static void move(int dir){
+    static int[][] move(int dir, int[][]board){
         if(dir == 1){  // 위로
-            for(int s =0; s<board.length-1; s++) {
-                for (int g = board[0].length - 1; g > 0; g--) {
-                    if (board[s][g] == 0) {
-                        for (int m = s; m < board.length - 1; m++) {
-                            board[m][g] = board[m + 1][g];
-                        }
-                        board[board.length - 1][g] = 0;
+
+            for(int s =1; s<board.length; s++){
+                for(int g =0; g<board[0].length; g++){
+                    int i=0;
+                    while(s-i-1>=0){
+                        if(board[s-i-1][g]!=0) break;
+                        board[s-i-1][g] = board[s-i][g];
+                        board[s-i][g] =0;
+                        i++;
                     }
                 }
             }
@@ -105,13 +105,15 @@ public class b12100_시뮬레이션_2048 {
         }
 
         else if(dir == 2){  // 아래로
-            for(int s =0; s<board.length; s++){
-                for(int g = board[0].length-1; g>0; g--){
-                    if(board[s][g]==0){
-                        for(int m = s; m>0; m--){
-                            board[m][g] = board[m-1][g];
+            for(int s = board.length-1; s>0; s--){
+                for(int g=0; g<board[0].length; g++){
+                    int i=0;
+                    while(s+i<board.length){
+                        if(board[s+i][g]==0){
+                            board[s+i][g] = board[s+i-1][g];
+                            board[s+i-1][g] =0;
                         }
-                        board[0][g] = 0;
+                        i++;
                     }
                 }
             }
@@ -131,17 +133,18 @@ public class b12100_시뮬레이션_2048 {
         }
 
         else if(dir == 3){ // 좌로
-            for(int s =0; s<board.length; s++){
-                for(int g = 0; g<board[0].length-1; g++){
-                    if(board[s][g]==0){
-                        for(int m = g; m<board[0].length-1; m++){
-                            board[s][m] = board[s][m+1];
+            for(int s =0; s<board.length; s++) {
+                for (int g = 0; g < board[0].length; g++) {
+                    int i =0;
+                    while(g-i-1>=0){
+                        if(board[s][g-i-1]==0){
+                            board[s][g-i-1] = board[s][g-i];
+                            board[s][g-i] =0;
                         }
-                        board[s][board[0].length-1] = 0;
+                        i++;
                     }
                 }
             }
-
             for(int s =0; s<board.length; s++){
                  for(int g=0; g<board[0].length-1; g++) {
                     if(board[s][g] == board[s][g+1]){
@@ -157,22 +160,18 @@ public class b12100_시뮬레이션_2048 {
         }
 
         else if(dir == 4){ // 우
-            for(int s =0; s<board.length; s++){
-                for(int g = board[0].length-1; g>0; g--){
-                    if(board[s][g]==0){
-
-                        ///
-                        for(int m = g; m>0; m--){
-                            board[s][m] = board[s][m-1];
+            for(int s =0; s<board.length; s++) {
+                for (int g = board[0].length; g > 0; g--) {
+                    int i=0;
+                    while(g+i<board[0].length){
+                        if(board[s][g+i]==0){
+                            board[s][g+i] = board[s][g+i-1];
+                            board[s][g+i-1] =0;
                         }
-                        board[s][0] = 0;
-                        ////
-
-
+                        i++;
                     }
                 }
             }
-
             for(int s =0; s<board.length; s++){
                 for(int g=board[0].length-1; g>0; g--) {
                     if(board[s][g] == board[s][g-1]){
@@ -186,5 +185,6 @@ public class b12100_시뮬레이션_2048 {
                 }
             }
         }
+        return board;
     }
 }
