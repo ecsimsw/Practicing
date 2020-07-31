c060695eb9ec1e961f00e1477c5bd4dd8063bc50
ecsimsw <ecsimsw@gmail.com>
2020-08-01 07:47:17 +0900

    하루 한알 / b1978_소수_소수찾기

--- src/BOJ/마구풀기.java
+++ src/BOJ/마구풀기.java
@@ -3,6 +3,66 @@ package BOJ;
 import java.io.*;
 import java.util.*;
 
+class b1978_소수_소수찾기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+
+        int cnt=0;
+
+        String[] numstr = br.readLine().split(" ");
+
+        int index =0;
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(numstr[i]);
+            if(num == 1) continue;
+            if(num == 2 || num == 3 || num == 5 || num == 7 || num == 11 || num ==13){
+                cnt++;
+                continue;
+            }
+            if(num <= 13) continue;
+            nums[index++] = num;
+        }
+
+        Arrays.sort(nums);
+
+        int[] prime = new int[1000];
+        int primeCnt =0;
+        for(int i=2; i<=1001; i++){
+            if(i % 2 ==0|| i % 3 ==0|| i % 5==0 || i %7==0 || i% 11==0 || i %13==0){
+                continue;
+            }
+            boolean isPrime = true;
+            for(int p=0; p<primeCnt; p++){
+                if(i%prime[p]==0){
+                    isPrime = false;
+                }
+            }
+            if(isPrime){
+                prime[primeCnt++] = i;
+            }
+
+            if(i == nums[nums.length-1]) break;
+        }
+
+        for(int i : nums){
+            boolean isPrime = true;
+            for(int p :prime ){
+                if(i<p)break;
+                if(i == p){
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        System.out.print(cnt);
+
+    }
+}
+
+
 class b1003_재귀_피보나치 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
