b96b158193c544eb1471bdc311aa6ddc9b10a425
ecsimsw <ecsimsw@gmail.com>
2020-08-07 23:57:18 +0900

    하루 한알 / b5585_그리디_거스름돈

--- src/BOJ/마구풀기.java
+++ src/BOJ/마구풀기.java
@@ -3,6 +3,31 @@ package BOJ;
 import java.io.*;
 import java.util.*;
 
+class b5585_그리디_거스름돈{
+    static public void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] moneyList = new int[]{500,100,50,10,5,1};
+
+        int n = 1000-Integer.parseInt(br.readLine());
+
+        int cnt=0;
+        int moneyIndex= 0;
+        while(true){
+            if(n>=moneyList[moneyIndex]){
+                cnt++;
+                n-=moneyList[moneyIndex];
+            }
+            else{
+                moneyIndex++;
+            }
+            if(n ==0) break;
+        }
+        System.out.print(cnt);
+    }
+}
+
+
 class b1085{
     static public void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
