7c5691cee092698d62a8589eb6359a1edd2d6f71
ecsimsw <ecsimsw@gmail.com>
2020-09-04 13:38:13 +0900

    하루 한알 / b10815_이분탐색_숫자카드

--- src/BOJ/마구풀기.java
+++ src/BOJ/마구풀기.java
@@ -3,6 +3,38 @@ package BOJ;
 import java.io.*;
 import java.util.*;
 
+class b10815_숫자카드{
+    static public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] nums = br.readLine().split(" ");
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            map.put(Integer.parseInt(nums[i]), true);
+        }
+
+        int n_ = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        nums = br.readLine().split(" ");
+        for(int i=0; i<n_; i++){
+
+            if(map.containsKey(Integer.parseInt(nums[i]))){
+                sb.append("1 ");
+            }
+
+            else{
+                sb.append("0 ");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
 class b5585_그리디_거스름돈{
     static public void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
