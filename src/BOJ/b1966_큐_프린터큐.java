c4e7b49b36d44a9e0c99f0d98dd7407998eca2b3
ecsimsw <ecsimsw@gmail.com>
2020-09-09 03:41:03 +0900

    b1966_큐_프린터큐

--- src/BOJ/b1966_큐_프린터큐.java
+++ src/BOJ/b1966_큐_프린터큐.java
@@ -10,46 +10,57 @@ public class b1966_큐_프린터큐 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = 6;
-        int index =0;
+        StringBuilder sb = new StringBuilder();
 
-        int[] nums = new int[]{1,1,9,1,1,1};
-        int[] p = new int[]{5,0,0,0,0,0,0,0,1};
-        int myIndex =index;
-        int max = 9;
-        int cnt =0;
+        int N = Integer.parseInt(br.readLine());
 
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i : nums){
-            queue.offer(i);
-        }
+        for(int loop=0; loop<N; loop++){
+            String[] str = br.readLine().split(" ");
+            int m = Integer.parseInt(str[0]);
+            int index =Integer.parseInt(str[1]);
 
-        while(!queue.isEmpty()){
-            int peek = queue.poll();
+            int[] p = new int[10];
+            int[] nums = new int[m];
 
-            if(peek == max){
-                if(myIndex == 0) {break;}
-                else{
-                    myIndex--;
-                    if(myIndex==0){
-                        myIndex = queue.size()-1;
-                    }
+            String[] num = br.readLine().split(" ");
+            int max = 0;
+            for(int n=0; n<m; n++){
+                int priority = Integer.parseInt(num[n]);
+                p[priority]++;
+                nums[n] = priority;
+
+                if(max < priority) {max = priority;}
+            }
+
+            int myIndex =index;
+            int cnt =0;
+
+            Queue<Integer> queue = new LinkedList<>();
+            for(int i : nums){ queue.offer(i); }
+
+            while(!queue.isEmpty()){
+                int peek = queue.poll();
+
+                if(peek == max){
                     cnt++;
-                    nums[max]--;
-                    max = findMax(nums);
+                    if(myIndex == 0) { break;}
+                    else{ p[max]--; max = findMax(p); }
                 }
+                else{ queue.offer(peek); }
+
+                myIndex--;
+                if(myIndex ==-1){ myIndex = queue.size()-1; }
             }
-            else{
-                queue.offer(peek);
-            }
+
+            sb.append(cnt+"\n");
         }
 
-        System.out.print(cnt);
+        System.out.print(sb);
     }
 
-    static int findMax(int[] nums){
+    static int findMax(int[] p){
         for(int i=9; i>0; i--){
-            if(nums[i] != 0) return i;
+            if(p[i] != 0) return i;
         }
         return 0;
     }
