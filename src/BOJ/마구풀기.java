23521b1b65c87d28cc17c305507fc5f7b52f8378
ecsimsw <ecsimsw@gmail.com>
2020-10-30 23:18:37 +0900

    하루 한알 / b1026_보물

--- src/BOJ/마구풀기.java
+++ src/BOJ/마구풀기.java
@@ -3,6 +3,37 @@ package BOJ;
 import java.io.*;
 import java.util.*;
 
+class b1026_보물{
+    static public void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        String[] nums = br.readLine().split(" ");
+        for(int i=0; i<n; i++){
+            a[i] = Integer.parseInt(nums[i]);
+        }
+
+        nums = br.readLine().split(" ");
+        for(int i=0; i<n; i++){
+            b[i] = Integer.parseInt(nums[i]);
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int sum =0;
+        for(int i=0; i<n; i++){
+            sum += (a[i] * b[n-i-1]);
+        }
+
+        System.out.println(sum);
+    }
+}
+
 class b3009_네번째점{
     static public void main(String[] args) throws Exception{
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
