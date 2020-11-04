8c413adde618ee24ce354ecee5055cc1e4b238ab
ecsimsw <ecsimsw@gmail.com>
2020-11-04 15:45:10 +0900

    하루 한알 / b4673_수학_셀프넘버

--- src/BOJ/b4673_수학_셀프넘버.java
+++ src/BOJ/b4673_수학_셀프넘버.java
@@ -5,8 +5,7 @@ import java.io.InputStreamReader;
 import java.nio.Buffer;
 
 public class b4673_수학_셀프넘버 {
-    public static void main(String[] args) throws Exception{
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
         N = 10000;
 
         arr = new boolean[N];
