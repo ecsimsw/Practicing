21c9c9ffd8aff3e2a32553ee7e59a936f903ac90
ecsimsw <ecsimsw@gmail.com>
2020-11-04 15:43:10 +0900

    하루 한알 / b4673_수학_셀프넘버

---
+++ src/BOJ/b4673_수학_셀프넘버.java
@@ -0,0 +1,45 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class b4673_수학_셀프넘버 {
+    public static void main(String[] args) throws Exception{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = 10000;
+
+        arr = new boolean[N];
+
+        for(int i=1; i<N; i++){
+            if(arr[i] == true) continue;
+            d(i);
+        }
+
+        for(int i=1; i<N; i++){
+            if(arr[i] == true) continue;
+            System.out.println(i);
+        }
+    }
+
+    static boolean[] arr;
+    static int N;
+
+    static void d(int n){
+        int sum = n;
+
+        while(n>=1){
+            sum += n%10;
+            n /= 10;
+        }
+        if(sum > N-1) return;
+
+        if(arr[sum] == true){
+            return;
+        }
+        else{
+            arr[sum] = true;
+            d(sum);
+        }
+    }
+}
