3b598778b7975585d9d482c44556e4728d20c261
ecsimsw <ecsimsw@gmail.com>
2020-11-05 11:34:01 +0900

    하루 한알 / b1780_분할정복_종이의개수

--- src/javaPracticing/Application.java
+++ src/javaPracticing/Application.java
@@ -3,27 +3,31 @@ package javaPracticing;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int x = 10;
-        int y = 20;
 
-//        MyClass x = new MyClass(10);
-//        MyClass y = new MyClass(20);
 
-        int[] swapR = swap(x, y);
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(5);
+        list.addFirst(5);
+        list.set(3,5);
+        if(br.readLine().equals("a")){
+            list.add(5);
+        }
+        Collections.sort(list);
 
-        x = swapR[0];
-        y = swapR[1];
+        Integer[] arr = list.toArray(new Integer[10]);
 
-        System.out.println(x + " "+y);
+        Integer[] arr_ = new Integer[]{1,5,6,74,5};
+
+        List list_ = new LinkedList(Arrays.asList(arr));
+
+        list_.add(1);
+
     }
 
     static int[] swap(int x, int  y){
