ce8ceb7b21036e188c1ce662833b3d27c49a22b2
ecsimsw <ecsimsw@gmail.com>
2020-11-06 11:28:46 +0900

    하루 한알 / b1764_이분탐색_듣보잡

---
+++ src/BOJ/b1764_이분탐색_듣보잡.java
@@ -0,0 +1,42 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class b1764_이분탐색_듣보잡 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+
+        HashSet<String> set1 = new HashSet<String>();
+        for(int i=0; i<n; i++){
+            set1.add(br.readLine());
+        }
+        HashSet<String> set2 = new HashSet<String>();
+        for(int i=0; i<m; i++){
+            set2.add(br.readLine());
+        }
+
+        LinkedList<String> answers = new LinkedList<>();
+        for(String s : set1){
+            if(set2.contains(s)){
+                answers.add(s);
+            }
+        }
+
+        Collections.sort(answers);
+        System.out.println(answers.size());
+        for(String s : answers) {
+            System.out.println(s);
+        }
+    }
+}
+
+
