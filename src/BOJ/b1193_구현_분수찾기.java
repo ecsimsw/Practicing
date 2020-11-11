d9b9bdf0d0c68fefd463c46a98ebc58e1175d3a6
ecsimsw <ecsimsw@gmail.com>
2020-11-11 13:56:38 +0900

    b1193_구현_분수찾기

---
+++ src/BOJ/b1193_구현_분수찾기.java
@@ -0,0 +1,45 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b1193_구현_분수찾기 {
+    static public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int index=0;
+
+        while(true){
+            if((index*index + index)/2 >= n){ break; }
+            index++;
+        }
+
+        int now = (index*index + index)/2 - index;
+        int dif = n - now;
+
+        int s;
+        int m;
+
+        if(index%2 !=0 ){
+            s = index;
+            m = 1;
+
+            for(int i=1; i<dif; i++){
+                s--;
+                m++;
+            }
+        }else{
+            s = 1;
+            m = index;
+
+            for(int i=1; i<dif; i++){
+                s++;
+                m--;
+            }
+        }
+        System.out.println(s+"/"+m);
+
+    }
+}
