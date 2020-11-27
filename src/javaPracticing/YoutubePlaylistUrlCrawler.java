82582bd4c86777474471ba34670aa188b376dd74
ecsimsw <ecsimsw@gmail.com>
2020-11-27 20:30:30 +0900

    fix

--- src/javaPracticing/YoutubePlaylistUrlCrawler.java
+++ src/javaPracticing/YoutubePlaylistUrlCrawler.java
@@ -6,12 +6,14 @@ import java.util.Scanner;
 
 public class YoutubePlaylistUrlCrawler {
     static String line;
+    // html에서 재생목록 링크를 포함하는 html 소스를 텍스트로 저장하고, 그 경로를 htmlFile로 둔다.
+    static String htmlFile = ".txt";
 
     public static void main(String[] args){
         StringBuilder sb = new StringBuilder();
 
         try{
-            File file = new File("fileDir");
+            File file = new File(htmlFile);
             Scanner scan = new Scanner(file);
 
             while(scan.hasNextLine()){
