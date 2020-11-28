36ef325e12edbd465df4d52af413baa7d8fdea85
ecsimsw <ecsimsw@gmail.com>
2020-11-28 22:27:37 +0900

    feat : 입력 문자열로 자동차 리스트 생성 구현

--- src/WoowarCoursePractice/racingCar/src/utils/CarListGenerator.java
+++ src/WoowarCoursePractice/racingCar/src/utils/CarListGenerator.java
@@ -1,4 +1,19 @@
 package WoowarCoursePractice.racingCar.src.utils;
 
+import WoowarCoursePractice.racingCar.src.domain.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CarListGenerator {
+    private CarListGenerator(){}
+
+    public static List<Car> createCarList(String nameLine, String nameSeparator){
+        List<Car> carList = new ArrayList<>();
+        for(String name : nameLine.split(nameSeparator)){
+            carList.add(new Car(name));
+        }
+        return carList;
+    }
 }
