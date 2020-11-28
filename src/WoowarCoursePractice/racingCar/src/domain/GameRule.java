32b613e9d9853bf2eb93ff47d469bda9f678bf4a
ecsimsw <ecsimsw@gmail.com>
2020-11-29 00:51:53 +0900

    feat : 자동차 리스트 중에서 가장 거리가 먼 것 찾기 구현

--- src/WoowarCoursePractice/racingCar/src/domain/GameRule.java
+++ src/WoowarCoursePractice/racingCar/src/domain/GameRule.java
@@ -1,5 +1,9 @@
 package WoowarCoursePractice.racingCar.src.domain;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameRule {
     private static final int CONDITION_VALUE_TO_MOVE = 4;
 
@@ -7,5 +11,28 @@ public class GameRule {
 
     public static boolean isConditionToGo(int value){
         return value >= CONDITION_VALUE_TO_MOVE;
+    }
+
+    public static List<Car> findWinner(List<Car> carList){
+        int farthestPostion = findFarthestPositionWithStream(carList);
+        return carList.stream().filter(car -> car.position == farthestPostion)
+                .collect(Collectors.toList());
+    }
+
+    private static int findFarthestPositionWithStream(List<Car> carList){
+        return carList.stream()
+                .max(Comparator.comparingInt(car -> car.position))
+                .get()
+                .position;
+    }
+
+    private static int findFarthestPosition(List<Car> carList){
+        int max = 0;
+        for(Car car : carList){
+            if(max < car.position){
+                max = car.position;
+            }
+        }
+        return max;
     }
 }
