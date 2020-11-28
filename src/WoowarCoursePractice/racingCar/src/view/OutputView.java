62279d2b8d25ff1708543cb3bde202ac3cb8461c
ecsimsw <ecsimsw@gmail.com>
2020-11-29 00:53:08 +0900

    feat : 우승자 List의 이름 사이에 쉼표를 넣어 출력 기능 구현

--- src/WoowarCoursePractice/racingCar/src/view/OutputView.java
+++ src/WoowarCoursePractice/racingCar/src/view/OutputView.java
@@ -1,11 +1,16 @@
 package WoowarCoursePractice.racingCar.src.view;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.List;
+
 public class OutputView {
     private static final String RACING_RESULT_MESSAGE = "실행 결과";
     private static final String SEPARATOR_NAME_POSITION = " : ";
     private static final String POSITION_BAR = "-";
     private static final String SEPARATOR_CAR = "\n";
     private static final String SEPARATOR_ROUND = "\n";
+    private static final String ANNOUNCE_WINNER = "가 최종 우승 하였습니다.";
 
     private OutputView(){}
 
@@ -29,7 +34,15 @@ public class OutputView {
         printMsg(SEPARATOR_ROUND);
     }
 
-    public static void printMsg(String msg){
+    public static void printWinner(List<String> winnerName){
+        String collectNames = winnerName.stream()
+                .collect(Collectors.joining(","));
+
+        printMsg(collectNames);
+        printMsg(ANNOUNCE_WINNER);
+    }
+
+    public static void printMsg(String msg) {
         System.out.print(msg);
     }
 }
