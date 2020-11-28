6e42e620b6d4266f83ab98f757c216242f338c21
ecsimsw <ecsimsw@gmail.com>
2020-11-28 22:11:34 +0900

    feat : 사용자 메시지 출력 구현

--- src/WoowarCoursePractice/racingCar/src/view/OutputView.java
+++ src/WoowarCoursePractice/racingCar/src/view/OutputView.java
@@ -1,4 +1,10 @@
 package WoowarCoursePractice.racingCar.src.view;
 
 public class OutputView {
+
+    private OutputView(){}
+
+    public static void printMsg(String msg){
+        System.out.print(msg);
+    }
 }
