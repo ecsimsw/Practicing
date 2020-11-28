b20f602b1100a9b5cedd0202de2ea236cc379251
ecsimsw <ecsimsw@gmail.com>
2020-11-28 22:49:51 +0900

    feat : 자동차 게임 play 로직 구현

--- src/WoowarCoursePractice/racingCar/src/domain/RacingGame.java
+++ src/WoowarCoursePractice/racingCar/src/domain/RacingGame.java
@@ -1,4 +1,42 @@
 package WoowarCoursePractice.racingCar.src.domain;
 
+import WoowarCoursePractice.racingCar.src.view.OutputView;
+
+import java.util.List;
+
 public class RacingGame {
+    private final List<Car> carList;
+    private final int totalRound;
+
+    private int currentRound;
+
+    public RacingGame(List<Car> carList, int totalRound){
+        this.carList = carList;
+        this.totalRound = totalRound;
+        currentRound = 0;
+    }
+
+    public void play(){
+        do{
+            tryMovingCar();
+            printRoundScore();
+            endRound();
+        }while(isGameEnd());
+    }
+
+    private boolean isGameEnd(){
+        return totalRound == currentRound;
+    }
+
+    private void tryMovingCar(){
+        carList.stream().forEach(car-> car.tryMove());
+    }
+
+    private void printRoundScore(){
+
+    }
+
+    private void endRound(){
+        currentRound++;
+    }
 }
