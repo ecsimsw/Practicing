b598ab970b4286ce32a3d018bab837125ff29fdd
ecsimsw <ecsimsw@gmail.com>
2020-11-29 00:25:50 +0900

    feat : Application 구현 자동차와 라운드 수를 세팅하고 레이싱 게임을 play()

--- src/WoowarCoursePractice/racingCar/src/domain/Application.java
+++ src/WoowarCoursePractice/racingCar/src/domain/Application.java
@@ -1,10 +1,34 @@
 package WoowarCoursePractice.racingCar.src.domain;
 
+import WoowarCoursePractice.racingCar.src.utils.CarListGenerator;
+import WoowarCoursePractice.racingCar.src.view.InputView;
+
 import java.util.Scanner;
+import java.util.List;
 
 public class Application {
+    private static final String SEPARATOR_CAR_NAME = ",";
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        final List<Car> carList = setCarList(scanner);
+        final int totalRound = askRoundCount(scanner);
+
+        RacingGame racingGame = new RacingGame(carList, totalRound);
+        racingGame.play();
+    }
+
+    private static List<Car> setCarList(Scanner scanner){
+        return CarListGenerator.createCarList(askCarName(scanner), SEPARATOR_CAR_NAME);
+    }
+
+    private static String askCarName(Scanner scanner){
+        return InputView.askCarNames(scanner);
+    }
+
+    private static int askRoundCount(Scanner scanner){
+        String ans = InputView.askNumberOfRound(scanner);
+        return Integer.parseInt(ans);
     }
 }
