724323af94850d2b466cc4d99ba5365474887ff4
ecsimsw <ecsimsw@gmail.com>
2020-11-28 22:46:17 +0900

    feat : 자동차 전진 시도, GameRule 클래스로 전진 조건 확인 구현

--- src/WoowarCoursePractice/racingCar/src/domain/Car.java
+++ src/WoowarCoursePractice/racingCar/src/domain/Car.java
@@ -1,11 +1,24 @@
 package WoowarCoursePractice.racingCar.src.domain;
 
+import WoowarCoursePractice.racingCar.src.utils.RandomUtils;
+
 public class Car {
+    private static final int MAX_MOVE = 9;
+    private static final int MIN_MOVE = 0;
+
     private final String name;
     private int position;
 
     public Car(String name){
         this.name = name;
         this.position = 0;
+    }
+
+    public void tryMove(){
+        int value =  RandomUtils.nextInt(MIN_MOVE, MAX_MOVE);
+
+        if(GameRule.isConditionToGo(value)){
+            position++;
+        }
     }
 }
--- src/WoowarCoursePractice/racingCar/src/domain/GameRule.java
+++ src/WoowarCoursePractice/racingCar/src/domain/GameRule.java
@@ -1,4 +1,11 @@
 package WoowarCoursePractice.racingCar.src.domain;
 
 public class GameRule {
+    private static final int CONDITION_VALUE_TO_MOVE = 4;
+
+    private GameRule(){}
+
+    public static boolean isConditionToGo(int value){
+        return value >= CONDITION_VALUE_TO_MOVE;
+    }
 }
