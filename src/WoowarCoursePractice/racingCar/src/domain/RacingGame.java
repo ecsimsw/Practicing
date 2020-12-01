6dd004f8745b9cf672bb9c44d4fe2d3ebe59a57f
ecsimsw <ecsimsw@gmail.com>
2020-12-02 03:30:30 +0900

    refactor : RacingGame의 시도 횟수를 저장하는 변수 round를 Rounds 클래스로 포장

--- src/WoowarCoursePractice/racingCar/src/domain/RacingGame.java
+++ src/WoowarCoursePractice/racingCar/src/domain/RacingGame.java
@@ -2,19 +2,13 @@ package WoowarCoursePractice.racingCar.src.domain;
 
 import WoowarCoursePractice.racingCar.src.view.OutputView;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class RacingGame {
     private final Cars cars;
-    private final int totalRound;
-
-    private int currentRound;
+    private final Rounds rounds;
 
     public RacingGame(Cars cars, int totalRound){
         this.cars = cars;
-        this.totalRound = totalRound;
-        currentRound = 0;
+        rounds = new Rounds(totalRound);
     }
 
     public void play(){
@@ -22,8 +16,8 @@ public class RacingGame {
         do{
             cars.move();
             printRoundScore();
-            endRound();
-        }while(!isGameEnd());
+            rounds.endRound();
+        }while(!rounds.isEndAllRounds());
         printWinner();
     }
 
@@ -32,20 +26,7 @@ public class RacingGame {
         OutputView.printRoundSeparator();
     }
 
-    private void endRound(){
-        currentRound++;
-    }
-
-    private boolean isGameEnd(){
-        return totalRound == currentRound;
-    }
-
     private void printWinner(){
-        List<String> winnerNames = getWinnerNames();
-        OutputView.printWinner(winnerNames);
-    }
-
-    private List getWinnerNames(){
-        return cars.getWinnerNames();
+        OutputView.printWinner(cars.getWinnerNames());
     }
 }
