0a5bb1935a9542b42cbcd8ed9e4b1b17e7e23a99
ecsimsw <ecsimsw@gmail.com>
2020-12-01 12:57:10 +0900

    feat : 단위 게임이 종료되면 재시작 여부를 묻고 재시작 처리

--- src/WoowarCoursePractice/baseballGame/baseball/Application.java
+++ src/WoowarCoursePractice/baseballGame/baseball/Application.java
@@ -1,10 +1,23 @@
 package WoowarCoursePractice.baseballGame.baseball;
 
+import WoowarCoursePractice.baseballGame.view.InputView;
+
 import java.util.Scanner;
 
 public class Application {
+    private static final int PLAY_BUTTON_AGAIN = 1;
+    private static final int PLAY_BUTTON_END = 2;
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
+        do{
+            BaseballGame baseballGame = new BaseballGame(scanner);
+            baseballGame.play();
+        }while(checkPlayAgain(scanner));
+    }
+
+    private static boolean checkPlayAgain(Scanner scanner){
+        return InputView.getPlayAgainButton(scanner) == PLAY_BUTTON_AGAIN;
     }
 }
