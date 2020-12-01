eece40b22773d982bdb508ffa97b3d9fbceb9867
ecsimsw <ecsimsw@gmail.com>
2020-12-01 10:00:55 +0900

    feat : 임의의 3개 숫자로 구성된 targetNumber 생성 기능

--- src/WoowarCoursePractice/baseballGame/baseball/BaseballGame.java
+++ src/WoowarCoursePractice/baseballGame/baseball/BaseballGame.java
@@ -1,4 +1,38 @@
 package WoowarCoursePractice.baseballGame.baseball;
 
+import WoowarCoursePractice.baseballGame.utils.Numbers;
+import WoowarCoursePractice.baseballGame.utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
+
+    public BaseballGame(){
+
+    }
+
+    public void play(){
+        setTargetNumber();
+    }
+
+    private void setTargetNumber(){
+        List<Integer> targetList = new ArrayList<>();
+
+        while(targetList.size() < Numbers.LEN){
+            int rand = getRandWithoutDuplicate(targetList);
+            targetList.add(rand);
+        }
+    }
+
+    private int getRandWithoutDuplicate(List list){
+        int rand;
+
+        do{
+            rand = RandomUtils.nextInt(Numbers.MIN, Numbers.MAX);
+        }while(list.contains(rand));
+
+        return rand;
+    }
 }
+
