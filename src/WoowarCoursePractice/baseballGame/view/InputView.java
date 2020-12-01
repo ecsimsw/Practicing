05eb3c0a45d839b9fa2e26cc97b25e78b2a50ddf
ecsimsw <ecsimsw@gmail.com>
2020-12-01 10:06:18 +0900

    feat : 사용자 숫자 입력 기능 구현

--- src/WoowarCoursePractice/baseballGame/view/InputView.java
+++ src/WoowarCoursePractice/baseballGame/view/InputView.java
@@ -1,5 +1,24 @@
 package WoowarCoursePractice.baseballGame.view;
 
+import WoowarCoursePractice.baseballGame.utils.Numbers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.List;
+
 public class InputView {
-    
+    private static final String ASK_NUMBER_MESSAGE = "숫자를 입력하시오. ";
+
+    public static Numbers getNumbers(Scanner scanner) {
+        OutputView.printMsg(ASK_NUMBER_MESSAGE);
+
+        String input = scanner.nextLine();
+
+        List numberList = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        return new Numbers(numberList);
+    }
 }
--- src/WoowarCoursePractice/baseballGame/view/OutputView.java
+++ src/WoowarCoursePractice/baseballGame/view/OutputView.java
@@ -1,4 +1,9 @@
 package WoowarCoursePractice.baseballGame.view;
 
 public class OutputView {
+    private OutputView(){}
+
+    public static void printMsg(Object msg){
+        System.out.print(msg);
+    }
 }
