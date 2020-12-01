258bd6bb22094011386271bad58b702ba3169501
ecsimsw <ecsimsw@gmail.com>
2020-12-01 10:02:11 +0900

    feat : 숫자 리스트를 포장할 Numbers 클래스 정의

---
+++ src/WoowarCoursePractice/baseballGame/utils/Numbers.java
@@ -0,0 +1,15 @@
+package WoowarCoursePractice.baseballGame.utils;
+
+import java.util.List;
+
+public class Numbers {
+    public static int MIN = 1;
+    public static int MAX = 9;
+    public static int LEN = 3;
+
+    List<Integer> numberList;
+
+    public Numbers(List numberList){
+        this.numberList = numberList;
+    }
+}
