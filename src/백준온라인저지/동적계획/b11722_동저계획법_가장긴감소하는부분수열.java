5761cf55da5918d1bb257be9bd32387704cccf96
ecsimsw <ecsimsw@gmail.com>
2021-01-04 23:54:16 +0900

    하루 한알 : b11722_동저계획법_가장긴감소하는부분수열

---
+++ src/백준온라인저지/동적계획/b11722_동저계획법_가장긴감소하는부분수열.java
@@ -0,0 +1,42 @@
+package 백준온라인저지.동적계획;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b11722_동저계획법_가장긴감소하는부분수열 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        String[] strs = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(strs[i]);
+        }
+
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[i] > dp[j] + 1 && arr[j] > arr[i]) {
+                    dp[i] = dp[j]++;
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print(dp[i] + " ");
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.print("\n" + max);
+    }
+}
