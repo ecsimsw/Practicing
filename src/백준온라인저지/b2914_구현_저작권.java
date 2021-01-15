90ad38157bd7540dc276839ba047d3c92811a237
ecsimsw <ecsimsw@gmail.com>
2021-01-15 20:51:30 +0900

    하루 한알 / b10886_구현_notCuteOrCute Mac 환경 적응하기

--- src/백준온라인저지/b2914_구현_저작권.java
+++ src/백준온라인저지/b2914_구현_저작권.java
@@ -13,3 +13,22 @@ public class b2914_구현_저작권 {
         System.out.println(A*(I-1)+1);
     }
 }
+
+class b10886_구현_notCuteOrCute{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] votes = new int[2];
+
+        for(int i=0; i<n; i++){
+            votes[Integer.parseInt(br.readLine())]++;
+        }
+
+        if(votes[0] < votes[1]){
+            System.out.println("Junhee is cute!");
+        }else{
+            System.out.println("Junhee is not cute!");
+        }
+    }
+}
