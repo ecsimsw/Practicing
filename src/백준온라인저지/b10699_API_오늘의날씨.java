1ce1ad84c301991c9a66c0d3cf4563f2813bb556
ecsimsw <ecsimsw@gmail.com>
2021-01-16 22:31:25 +0900

    하루 한알 / b10699_API_오늘의날씨 Mac 환경 적응하기

---
+++ src/백준온라인저지/b10699_API_오늘의날씨.java
@@ -0,0 +1,15 @@
+package 백준온라인저지;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class b10699_API_오늘의날씨 {
+    public static void main(String[] args)throws Exception{
+        LocalDateTime ldt = LocalDateTime.now();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String string = ldt.format(dateTimeFormatter);
+
+        System.out.println(string);
+    }
+}
