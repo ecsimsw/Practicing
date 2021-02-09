6a7ae2597d70d6fb4d9e3086946389d09bbc0c3f
ecsimsw <ecsimsw@gmail.com>
2021-02-09 23:36:54 +0900

    하루 한알 / 문자열_ps 리팩토링

--- src/알고리즘문제해결전략/ProgrammerString.java
+++ src/알고리즘문제해결전략/ProgrammerString.java
@@ -1,9 +1,5 @@
 package 알고리즘문제해결전략;
 
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.util.HashMap;
-
 public class ProgrammerString {
     private static int[] arr = new int[26];
     private static char[] charArr;
@@ -11,31 +7,25 @@ public class ProgrammerString {
     private static int endIndex = 0;
 
     public static void main(String[] args) throws Exception {
-        String s = "programmerxxxprozmerqgram";
+        String s = "progxrammerrxproxgrammer";
         charArr = s.toCharArray();
 
         int first = 0;
         set();
         while (!check()) {
-            System.out.println(startIndex + " : " + endIndex);
             forwardEnd();
             first = endIndex;
         }
         first++;
-        System.out.println(first);
 
         int last = 0;
         set();
         startIndex = charArr.length - 1;
         endIndex = charArr.length - 1;
         while (!check()) {
-            System.out.println(startIndex + " : " + endIndex);
             backStart();
             last = startIndex;
         }
-
-
-        System.out.println(last);
 
         System.out.println(last - first + 1);
     }
@@ -62,7 +52,6 @@ public class ProgrammerString {
         }
         return false;
     }
-
 
     private static void set() {
         arr['p' - 'a'] = 1;
