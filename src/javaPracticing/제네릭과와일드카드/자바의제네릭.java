a135b0ee4c879cc4155d8fee5103dc3fececb0c2
ecsimsw <ecsimsw@gmail.com>
2021-02-22 22:53:52 +0900

    자바 / 자바의 제네릭

--- src/javaPracticing/제네릭과와일드카드/자바의제네릭.java
+++ src/javaPracticing/제네릭과와일드카드/자바의제네릭.java
@@ -4,22 +4,17 @@ public class 자바의제네릭 {
 
     public static void main(String[] args) {
 
-        Parent<Integer> p1 = new Parent<>();
+        Parent<Child> p1 = new Parent<>();
         Parent<Child> p2 = new Child();
-
-        p2.print();
-        p2.print(new Child());
-
         Child<Child> p3 = new Child();
-        p3.print(new Child());
+
+        p1.print(new Child()); // 1
+        p2.print(new Child()); // 1
+        p3.print(new Child()); // 2
     }
 }
 
 class Parent<T> {
-    public void print() {
-        System.out.println("이것은 Parent다");
-    }
-
     public T print(T args) {
         System.out.println("1 : " + args + "");
         return args;
@@ -27,11 +22,6 @@ class Parent<T> {
 }
 
 class Child<T extends Child> extends Parent {
-    @Override
-    public void print() {
-        System.out.println("이것은 child다");
-    }
-
     public T print(T args) {
         System.out.println("2 : " + args + "");
         return args;
