c7bc51a5830d74ac7b1c233916dc4a9525fd2018
ecsimsw <ecsimsw@gmail.com>
2021-03-07 03:47:57 +0900

    자바 / 상속보다는 조합을 사용해라

--- src/javaPracticing/상속보단조합/문제점.java
+++ src/javaPracticing/상속보단조합/문제점.java
@@ -1,45 +1,123 @@
 package javaPracticing.상속보단조합;
 
+import java.util.List;
+
 public class 문제점 {
 
     public static void main(String[] args) {
         EncryptedDocument encryptedDocument = new EncryptedDocument("value");
-        System.out.println(encryptedDocument.content());
-        System.out.println(encryptedDocument.length());
+
+    }
+}
+
+class Player {
+    private final List<Integer> cards;
+
+    public Player(final List<Integer> cards) {
+        this.cards = cards;
+    }
+
+    public int getCard() {
+        return cards.get(0);
+    }
+}
+
+class Dealer {
+
+    private final Player player;
+
+    public Dealer(final List<Integer> cards) {
+        player = new Player(cards);
+    }
+
+    public int distributeCard() {
+        return player.getCard();
     }
 }
 
 class Document {
-    final String value;
-    private int length;
+    private final String content;
 
-    public Document(String value) {
-        this.value = value;
+    public Document(String content) {
+        System.out.println(content);
+        this.content = content;
     }
 
-    public int length() {
-        return content().length();
-    }
-
-    String content() {
-        return value;
+    public int contentLength() {
+        return content.length();
     }
 }
 
-class EncryptedDocument extends Document {
+class EncryptedDocument {
+    private final Document document;
 
-    public EncryptedDocument(String value) {
-        super(value);
+    public EncryptedDocument(String content) {
+        document = new Document(content);
     }
 
-    @Override
-    public String toString() {
-        return content();
+    public String getLastCharacter() {
+        return content().substring(document.contentLength() - 1, document.contentLength());
     }
 
-    @Override
     public String content() {
-        String decryptedValue = "something";
-        return decryptedValue;
+        // document 해독
+        return "something";
+    }
+}
+
+class temp {
+    public static void main(String[] args) {
+        Sub sub = new Sub();
+    }
+}
+
+class Score {
+    public int score = 0;
+
+    public Score() {
+        addScore(1);
+        System.out.println(score);
+    }
+
+    public void addScore(int i) {
+
+    }
+}
+
+class MathScore extends Score {
+
+    public MathScore() {
+        addScore(1);
+        System.out.println(score);
+    }
+
+    @Override
+    public void addScore(int i) {
+        score += i;
+    }
+}
+
+class Super {
+    public Integer i = null;
+
+    public Super() {
+        printScore();
+    }
+
+    public void printScore() {
+
+    }
+}
+
+class Sub extends Super {
+
+    public Sub() {
+        i = 1;
+        printScore();
+    }
+
+    @Override
+    public void printScore() {
+        System.out.println(i);
     }
 }
